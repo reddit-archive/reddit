c783b295eb6cfad9726b2336c3e71bdc0ff5e25c
ketralnis <ketralnis@reddit.com>
2008-11-06 11:39:46 -0800

    typo in hide_score

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -280,7 +280,7 @@ class Link(Thing, Printable):
                 item.hide_score = True
             elif c.user == item.author:
                 item.hide_score = False
-            elif item._date < timeago("2 hours"):
+            elif item._date > timeago("2 hours"):
                 item.hide_score = True
             else:
                 item.hide_score = False
