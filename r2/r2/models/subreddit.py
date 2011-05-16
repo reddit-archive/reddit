e6178bedb1efcfc1ca4e36daa395b201c7aaf8c6
westmaas <westmaas@gmail.com>
2011-05-16 12:16:43 -0700

    Make default header image URL configurable.

--- r2/r2/models/subreddit.py
+++ r2/r2/models/subreddit.py
@@ -737,7 +737,7 @@ class _DefaultSR(FakeSubreddit):
     #notice the space before reddit.com
     name = ' reddit.com'
     path = '/'
-    header = 'http://static.reddit.com/reddit.com.header.png'
+    header = g.default_header_url
 
     def get_links_sr_ids(self, sr_ids, sort, time):
         from r2.lib.db import queries
