b7e6aa008aa2b3d616c7a45ecabc1c66251f5daa
Paradox <paradox460@gmail.com>
2011-07-14 13:31:51 -0700

    Options expandos on links, cleaned up old parts of CSS, added icons

--- r2/r2/public/static/js/compact.js
+++ r2/r2/public/static/js/compact.js
@@ -1,5 +1,12 @@
 /*This hides the url bar on mobile*/
 (function($) {
+    /*
+    Creates an element on the body that works to create a modal box
+    The callback function runs when the cover is clicked
+    Use it, for example, to hide your modal box.
+
+    It is kind of tricky to use on mobile platforms, subject to many odd bugs, likely caused by the way mobile platforms handle z-index
+    */
     function tool_cover(callback) {
         var toolcover = $("#toolcover");
         if (toolcover.length == 0) {
@@ -7,149 +14,149 @@
             toolcover = $("#toolcover");
         }
         toolcover.css("height", $(document).height())
-            .show().one("click", function() {
-            $(this).hide();
-            if (callback) callback();
-            return false; 
-        });
-    };
+                .show().one("click", function() {
+                    $(this).hide();
+                    if (callback) callback();
+                    return false;
+                });
+    }
+
+    ;
     $.fn.show_toolbar = function() {
         var tb = this;
-        tool_cover(function() { $(tb).hide(); });
         $(this).show();
-    };
-    $.fn.hide_toolbar = function() {
-        $("#toolcover").click();
     };
     $.unsafe_orig = $.unsafe;
     $.unsafe = function(text) {
         /* inverts websafe filtering of reddit app. */
         text = $.unsafe_orig(text);
-        if(typeof(text) == "string") {
+        if (typeof(text) == "string") {
             /* space compress the result */
             text = text.replace(/[\s]+/g, " ")
-                .replace(/> +/g, ">")
-                .replace(/ +</g, "<");
+                    .replace(/> +/g, ">")
+                    .replace(/ +</g, "<");
         }
         return (text || "");
     }
 })(jQuery);
 
 $(function() {
-    if($(window).scrollTop() == 0) {
+    if ($(window).scrollTop() == 0) {
         $(window).scrollTop(1);
-    };
+    }
+    ;
     /* Top menu dropdown*/
-    $('#topmenu_toggle').click ( function() {
+    $('#topmenu_toggle').click(function() {
         $(this).toggleClass("active");
         $('#top_menu').toggle();
         return false;
     });
+    //Self text expando
     $('.expando-button').live('click', function() {
         $(this).toggleClass("expanded");
         $(this).thing().find(".expando").toggle();
         return false;
     });
+    //Help expando
     $('.help-toggle').live('click', function() {
         $(this).toggleClass("expanded");
         $(this).parent().siblings(".markhelp-parent").toggle();
         return false;
     });
-   /*Options dropdown for each comment */
-   $('.thing_options_link').live('click', function() {
-       $(this).toggleClass("active");
-       $(this).siblings(".thing_options_popup_container")
-           .toggleClass("hidden");
-       $(this).siblings(".thing_options_popup_container")
-           .find(".thing_suboption_popup_container").addClass("hidden");
-       return false;
-   });
-   /*Sub-menus. This can be replicated infinitely.*/
-   $('.thing_suboption_link').live('click', function() {
-       $(this).prev(".thing_suboption_popup_container").toggleClass("hidden");
-       return false;
-   });
-   /*Collapse menu on click of .option_close*/
-   $('.thing_option_close').live('click', function() {
-       $(this).parents(".thing_options_popup_container,.thing_suboption_popup_container").addClass("hidden");
-       return false;
-   });
-   /*Added expansion to text-area (like facebook/Buzz/Twitter)*/
-   /*$('.usertext textarea').autoResize({
-           animate: false,
-               extraSpace : 20
-               });*/
 
-   $(".options_triangle .button").live("click", function(evt) {
-       $(this).parents(".options_triangle").hide_toolbar();
-   });
-   $('.triangle_link').live('click', function(evt) {
-       $(this).siblings(".options_triangle_parent")
-           .children(".options_triangle").show_toolbar();
-       return false;
-   });
-   /* the iphone doesn't play nice with live() unless there is already a registered click function.  That's sad */
-   $(".thing").click(function() {});
+    //Options expando
+    $('.options_link').live('click', function(evt) {
 
-   $(".link").live("click", function(evt) {
-       if(evt && evt.target && $(evt.target).hasClass("thing")) {
-           $(this).find(".triangle_link").click();
-           return false;
-       }
-   });
+        if (! $(this).siblings(".options_expando").hasClass('expanded')) {
+            $('.options_expando.expanded').each(function() { //Collapse any other open ones
+                $(this).removeClass('expanded');
+            });
+            $('.options_link.active').each(function() {
+               $(this).removeClass('active');
+            });
+            $(this).siblings(".options_expando").addClass('expanded'); //Expand this one
+            $(this).addClass('active');
+        } else {
+             $(this).siblings(".options_expando").removeClass('expanded'); //Just collapse this one
+             $(this).removeClass('active');
+        }
+        return false;
+    });
+    //Save button state transition
+    $(".save-button").live("click", function() {
+        $(this).toggle();
+        $(this).siblings(".unsave-button").toggle();
+    });
+    $(".unsave-button").live("click", function() {
+        $(this).toggle();
+        $(this).siblings(".save-button").toggle();
+    });
+    /* the iphone doesn't play nice with live() unless there is already a registered click function.  That's sad */
+    $(".thing").click(function() {
+    });
+
+    $(".link").live("click", function(evt) {
+        if (evt && evt.target && $(evt.target).hasClass("thing")) {
+            $(this).find(".options_link").click();
+            return false;
+        }
+    });
+    //Comment options
     $(".comment > .entry, .message > .entry").live("click", function(evt) {
         var foo = (evt) ? $(evt.target) : null;
         var thing = $(this).parent();
-        if(thing.hasClass("active")) {
+        if (thing.hasClass("active")) {
             thing.removeClass("active");
             return false;
-        } 
+        }
         /* collapsed messages/comments should uncolapse */
-        else if(thing.hasClass("collapsed")) {
+        else if (thing.hasClass("collapsed")) {
             thing.removeClass("collapsed");
             return false;
-        } 
+        }
         /* unread messages should be marked read */
-        else if(thing.hasClass("unread") || thing.hasClass("new")) {
+        else if (thing.hasClass("unread") || thing.hasClass("new")) {
             read_thing(thing);
             return false;
         }
         /* otherwise, fire a menu */
-        else if(foo &&
-                foo.filter("a").length == 0 && 
-                !foo.hasClass("arrow") && 
-                !foo.hasClass("button") && 
+        else if (foo &&
+                foo.filter("a").length == 0 &&
+                !foo.hasClass("arrow") &&
+                !foo.hasClass("button") &&
                 foo.filter("textarea").length == 0) {
-            thing.find(".triangle_link:first").click();
+            thing.find(".options_link:first").click();
             return false;
         }
-       });
-   /*Finally*/
-    $('a[href=#]').live('click', function() { return false; } );
+    });
+    /*Finally*/
+    $('a[href=#]').live('click', function() {
+        return false;
+    });
 });
 
 $(function() {
-        var eut = edit_usertext;
-        edit_user_text = function(what) {
-            $(what).parent().parent().toggleClass('hidden');
-            return eut(what);
-        };
+    var eut = edit_usertext;
+    edit_user_text = function(what) {
+        $(what).parent().parent().toggleClass('hidden');
+        return eut(what);
+    };
 
-    });
+});
 
 function showcover() {
     $.request("new_captcha");
     $(".login-popup:first").fadeIn()
-        .find(".popup").css("top", $(window).scrollTop() + 75).end()
-        .find(".cover").css("height", $(document).height()).end()
-        .find("form input[name=reason]").val(reason || "");
+            .find(".popup").css("top", $(window).scrollTop() + 75).end()
+            .find(".cover").css("height", $(document).height()).end()
+            .find("form input[name=reason]").attr("value", (reason || ""));
     return false;
 }
 
 function hidecover(where) {
     $(where).parents(".cover-overlay").fadeOut();
     return false;
-};
+}
 
 function show_edit_usertext(form) {
     var edit = form.find(".usertext-edit");
@@ -160,8 +167,8 @@ function show_edit_usertext(form) {
     edit.show();
 
     form
-        .find(".cancel, .save").show().end()
-        .find(".help-toggle").show().end();
+            .find(".cancel, .save").show().end()
+            .find(".help-toggle").show().end();
 
     textarea.focus();
 }
@@ -172,33 +179,34 @@ function fetch_more() {
 
     var o = document.location;
     var path = o.pathname.split(".");
-    if (path[path.length-1].indexOf('/') == -1) {
+    if (path[path.length - 1].indexOf('/') == -1) {
         path = path.slice(0, -1).join('.');
     } else {
         path = o.pathname;
     }
     var apath = o.protocol + "//" + o.host + path + ".json-compact" + o.search;
     var last = $("#siteTable").find(".thing:last");
-    apath += ((document.location.search) ? "&" : "?") + 
-        "after=" + last.thing_id();
+    apath += ((document.location.search) ? "&" : "?") +
+            "after=" + last.thing_id();
 
     if (last.find(".rank").length)
         "&count=" + parseInt(last.find(".rank").html())
 
     $.getJSON(apath, function(res) {
-            $.insert_things(res.data, true);
-            $(".thing").click(function() {});
-            /* remove the loading image */
-            $("#siteTable").next(".loading").remove();
-            if (res && res.data.length == 0) {
-                $(window).unbind("scroll");
-            }
+        $.insert_things(res.data, true);
+        $(".thing").click(function() {
         });
+        /* remove the loading image */
+        $("#siteTable").next(".loading").remove();
+        if (res && res.data.length == 0) {
+            $(window).unbind("scroll");
+        }
+    });
 }
 
 function toggle_collapse(elem) {
     $(elem).thing().addClass("collapsed").addClass("active")
-        .find('.thing_option_close:first').click();
-    return false; 
+            .find('.thing_option_close:first').click();
+    return false;
 }
 
