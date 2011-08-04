335111b94158bacdc3b43c652499bccd4793aa5f
Andre D <andre@andred.ca>
2011-08-04 13:02:08 -0400

    Do not subject login requests to validation of subreddit access.

--- r2/r2/controllers/reddit_base.py
+++ r2/r2/controllers/reddit_base.py
@@ -279,7 +279,7 @@ def set_subreddit():
             sr_name = chksrname(sr_name)
             if sr_name:
                 redirect_to("/reddits/search?q=%s" % sr_name)
-            elif not c.error_page:
+            elif not c.error_page and not request.path.startswith("/api/login/") :
                 abort(404)
     #if we didn't find a subreddit, check for a domain listing
     if not sr_name and isinstance(c.site, DefaultSR) and domain:
@@ -758,21 +758,21 @@ class RedditController(MinimalController):
         elif c.site == RandomNSFW:
             c.site = Subreddit.random_reddit(over18 = True)
             redirect_to("/" + c.site.path.strip('/') + request.path)
+        
+        if not request.path.startswith("/api/login/"):
+            # check that the site is available:
+            if c.site.spammy() and not c.user_is_admin and not c.error_page:
+                abort(404, "not found")
 
+            # check if the user has access to this subreddit
+            if not c.site.can_view(c.user) and not c.error_page:
+                abort(403, "forbidden")
 
-        # check that the site is available:
-        if c.site.spammy() and not c.user_is_admin and not c.error_page:
-            abort(404, "not found")
-
-        # check if the user has access to this subreddit
-        if not c.site.can_view(c.user) and not c.error_page:
-            abort(403, "forbidden")
-
-        #check over 18
-        if (c.site.over_18 and not c.over18 and
-            request.path not in  ("/frame", "/over18")
-            and c.render_style == 'html'):
-            return self.intermediate_redirect("/over18")
+            #check over 18
+            if (c.site.over_18 and not c.over18 and
+                request.path not in ("/frame", "/over18")
+                and c.render_style == 'html'):
+                return self.intermediate_redirect("/over18")
 
         #check whether to allow custom styles
         c.allow_styles = self.allow_stylesheets
