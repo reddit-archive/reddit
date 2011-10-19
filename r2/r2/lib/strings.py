59e3f6c9f2792c18ea57febde14742083d56f241
Max Goodman <max@reddit.com>
2011-10-19 16:24:19 -0700

    Update /reddits help text to the changed subscribe button text.

--- r2/r2/lib/strings.py
+++ r2/r2/lib/strings.py
@@ -106,7 +106,7 @@ string_dict = dict(
         moderator = _('below are the reddits that you have moderator access to.')
         ),
 
-    sr_subscribe =  _('click the `+frontpage` or `-frontpage` buttons to choose which reddits appear on your front page.'),
+    sr_subscribe =  _('click the `subscribe` or `unsubscribe` buttons to choose which reddits appear on your front page.'),
 
     searching_a_reddit = _('you\'re searching within the [%(reddit_name)s](%(reddit_link)s) reddit. '+
                            'you can also search within [all reddits](%(all_reddits_link)s)'),
