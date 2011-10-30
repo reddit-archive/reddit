4d2c3c2835df38874b2737358f927c2bd9803220
Neil Williams <neil@reddit.com>
2011-10-30 15:22:58 -0700

    Generate gzipped versions of JS/CSS static files.
    Also, flip the symlinking so that unmangled points to the latest
    mangled file.

--- r2/r2/lib/static.py
+++ r2/r2/lib/static.py
@@ -4,6 +4,7 @@ import os
 import hashlib
 import json
 import base64
+import shutil
 
 def generate_static_name(name, base=None, shorthash=None):
     """Generate a unique filename.
@@ -17,7 +18,7 @@ def generate_static_name(name, base=None, shorthash=None):
     name, ext = os.path.splitext(name)
     return name + '.' + shorthash + ext
 
-def update_static_names(names_file, files, make_links=False):
+def update_static_names(names_file, files):
     """Generate a unique file name mapping for ``files`` and write it to a
     JSON file at ``names_file``."""
     if os.path.exists(names_file):
@@ -27,20 +28,19 @@ def update_static_names(names_file, files, make_links=False):
 
     base = os.path.dirname(names_file)
     for path in files:
+        name = os.path.relpath(path, base)
+        mangled_name = generate_static_name(name, base)
+        names[name] = mangled_name
+
         if not os.path.islink(path):
-            name = os.path.relpath(path, base)
-            names[name] = generate_static_name(name, base)
+            mangled_path = os.path.join(base, mangled_name)
+            shutil.move(path, mangled_path)
+            os.symlink(mangled_name, path)
 
     json_enc = json.JSONEncoder(indent=2, sort_keys=True)
     open(names_file, "w").write(json_enc.encode(names))
 
-    if make_links:
-        for name, staticname in names.iteritems():
-            link = os.path.join(base, staticname)
-            if not os.path.exists(link):
-                os.symlink(name, link)
-
     return names
 
 if __name__ == "__main__":
-    update_static_names(sys.argv[1], sys.argv[2:], make_links=True)
+    update_static_names(sys.argv[1], sys.argv[2:])
