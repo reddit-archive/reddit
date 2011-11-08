777efab2dfe60e3a1204fd36bfd83130c3d56bdb
Max Goodman <max@reddit.com>
2011-11-08 12:43:02 -0800

    Move r.config into an r.setup() call after modules load.
    The inline script now has id="config" and is in JSONP format, which will
    make it easier for browser extensions and greasemonkey scripts to access
    reddit config variables in isolated environments.

--- r2/r2/public/static/js/base.js
+++ r2/r2/public/static/js/base.js
@@ -1,5 +1,11 @@
 r = window.r || {}
 
+r.setup = function(config) {
+    r.config = config
+    // Set the legacy config global
+    reddit = config
+}
+
 $(function() {
     r.login.ui.init()
     r.analytics.init()
