591d4fbdbab04aa551bb5971d1c8712a810279c6
Max Goodman <max@reddit.com>
2011-12-07 16:24:47 -0800

    Strip usernames of surrounding whitespace in the login api.

--- r2/r2/controllers/validator/validator.py
+++ r2/r2/controllers/validator/validator.py
@@ -868,6 +868,8 @@ class VThrottledLogin(VLogin):
         self.vlength = VLength("user", max_length=100)
         
     def run(self, username, password):
+        if username:
+            username = username.strip()
         username = self.vlength.run(username)
 
         self.vdelay.run()
