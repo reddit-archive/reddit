b1a53e57d3c02a116717a57f5fcfea3821c7de95
Dmitry Jemerov <yole@jetbrains.com>
2012-03-12 23:00:50 +0100

    admins see count of reports in link/comment JSON

--- r2/r2/lib/jsontemplates.py
+++ r2/r2/lib/jsontemplates.py
@@ -183,6 +183,17 @@ class ThingJsonTemplate(JsonTemplate):
                     - time.timezone)
         elif attr == "child":
             return CachedVariable("childlisting")
+
+        if attr in ["num_reports", "banned_by", "approved_by"]:
+            if c.user_is_loggedin and thing.subreddit.is_moderator(c.user):
+                if attr == "num_reports":
+                    return thing.reported
+                ban_info = getattr(thing, "ban_info", {})
+                if attr == "banned_by":
+                    return ban_info.get("banner")
+                elif attr == "approved_by":
+                    return ban_info.get("unbanner")
+
         return getattr(thing, attr, None)
 
     def data(self, thing):
@@ -270,6 +281,9 @@ class LinkJsonTemplate(ThingJsonTemplate):
                                                 selftext     = "selftext",
                                                 selftext_html= "selftext_html",
                                                 num_comments = "num_comments",
+                                                num_reports  = "num_reports",
+                                                banned_by    = "banned_by",
+                                                approved_by  = "approved_by",
                                                 subreddit    = "subreddit",
                                                 subreddit_id = "subreddit_id",
                                                 is_self      = "is_self", 
@@ -329,6 +343,9 @@ class CommentJsonTemplate(ThingJsonTemplate):
                                                 link_id      = "link_id",
                                                 subreddit    = "subreddit",
                                                 subreddit_id = "subreddit_id",
+                                                num_reports  = "num_reports",
+                                                banned_by    = "banned_by",
+                                                approved_by  = "approved_by",
                                                 parent_id    = "parent_id",
                                                 )
 
