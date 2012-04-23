00010251ab220d9e2c82507e0322a51e220e5c18
shlurbee <valerie@reddit.com>
2012-04-23 11:59:48 -0700

    Validate subreddit name when submitting a link
    This fixes the bug where manually entering a long subreddit name
    when submitting a link caused a 500 server error

--- r2/r2/controllers/validator/validator.py
+++ r2/r2/controllers/validator/validator.py
@@ -853,6 +853,10 @@ class VSubmitSR(Validator):
             self.set_error(errors.SUBREDDIT_REQUIRED)
             return None
 
+        if not chksrname(sr_name):
+            self.set_error(errors.SUBREDDIT_NOEXIST)
+            return None
+
         try:
             sr = Subreddit._by_name(str(sr_name).strip())
         except (NotFound, AttributeError, UnicodeEncodeError):
@@ -989,6 +993,9 @@ class VUrl(VRequired):
     def run(self, url, sr = None, resubmit=False):
         if sr is None and not isinstance(c.site, FakeSubreddit):
             sr = c.site
+        elif not chksrname(sr):
+            self.set_error(errors.SUBREDDIT_NOEXIST)
+            sr = None
         elif sr:
             try:
                 sr = Subreddit._by_name(str(sr))
