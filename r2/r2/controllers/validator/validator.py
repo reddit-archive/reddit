55f1149580d4d7ee9b50f0f5e9c4ed3854839cb5
shlurbee <valerie@reddit.com>
2012-04-24 11:02:14 -0700

    Better error handling in VSubmitParent validation
    Fixes log error - AttributeError: 'NoneType' object has no attribute 'subreddit_slow'

--- r2/r2/controllers/validator/validator.py
+++ r2/r2/controllers/validator/validator.py
@@ -824,7 +824,7 @@ class VSubmitParent(VByName):
                 link = parent
                 if isinstance(parent, Comment):
                     link = Link._byID(parent.link_id, data=True)
-                if c.user_is_loggedin and can_comment_link(link):
+                if link and c.user_is_loggedin and can_comment_link(link):
                     return parent
         #else
         abort(403, "forbidden")
