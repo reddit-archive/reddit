4a8893c838bb4f49fbc9ab716cee2c9b92a54952
Neil Williams <neil@reddit.com>
2012-07-30 15:31:41 -0700

    Increase strictness of ConfigValue.bool.
    Previously, it would treat anything that wasn't (case insensitively)
    equal to "true" as False. The new configparser will ensure that the
    value is either "true" or "false" and not "slkdjfljksdf", reducing the
    risk of accidental False by typo.

--- r2/r2/lib/configparse.py
+++ r2/r2/lib/configparse.py
@@ -21,6 +21,8 @@
 ###############################################################################
 
 class ConfigValue(object):
+    _bool_map = dict(true=True, false=False)
+
     @staticmethod
     def int(v, key=None, data=None):
         return int(v)
@@ -31,7 +33,10 @@ class ConfigValue(object):
 
     @staticmethod
     def bool(v, key=None, data=None):
-        return (v.lower() == 'true') if v else None
+        try:
+            return ConfigValue._bool_map[v.lower()]
+        except KeyError:
+            raise ValueError("Unknown value for %r: %r" % (key, v))
 
     @staticmethod
     def tuple(v, key=None, data=None):
