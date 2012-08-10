b71d8bf7491e08a652ff82cd971d4ed490309b2f
Jason Harvey <alienth@reddit.com>
2012-08-10 12:41:35 -0700

    Store a last_visit time on the Account thing.

--- r2/r2/models/account.py
+++ r2/r2/models/account.py
@@ -225,6 +225,9 @@ class Account(Thing):
 
         LastModified.touch(self._fullname, "Visit")
 
+        self.last_visit = int(time.time())
+        self._commit()
+
     def make_cookie(self, timestr=None):
         if not self._loaded:
             self._load()
