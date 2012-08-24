526764290832462ab9559311b5c5da622bcfa38c
Andre D <andre@andred.ca>
2012-08-24 11:37:34 -0500

    Errors: Add the ability to error with json

--- r2/r2/controllers/error.py
+++ r2/r2/controllers/error.py
@@ -165,7 +165,10 @@ class ErrorController(RedditController):
                      c.response.content = str(code)
                 return c.response
             elif c.render_style == "api":
-                c.response.content = "{\"error\": %s}" % code
+                if 'usable_error_content' in request.environ:
+                    c.response.content = request.environ['usable_error_content']
+                else:
+                    c.response.content = "{\"error\": %s}" % code
                 return c.response
             elif takedown and code == 404:
                 link = Link._by_fullname(takedown)
