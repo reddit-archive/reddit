a58624b8e695e1f5cb3b33988473c4947ca9880f
Max Goodman <max@reddit.com>
2012-09-13 23:18:47 -0700

    Reorder interest bar key press handling logic.

--- r2/r2/public/static/js/interestbar.js
+++ r2/r2/public/static/js/interestbar.js
@@ -22,15 +22,18 @@ r.ui.InterestBar.prototype = {
     keyPressed: function() {
         var query = this.$query.val()
         query = $.trim(query)
-        if (query != this._lastQuery) {
+        if (query == this._lastQuery) {
+            return
+        } else {
             this._lastQuery = query
-            this.$el.addClass('working')
-            this.queryChangedDebounced(query)
         }
 
-        if (!query) {
-            this.$el.removeClass('working error')
+        this.queryChangedDebounced(query)
+        if (query) {
+            this.$el.addClass('working')
+        } else {
             this.hideResults()
+            this.$el.removeClass('working error')
         }
     },
 
