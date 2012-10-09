47549b4bb35ca033b881ce84c66a906d1abf1594
Max Goodman <max@reddit.com>
2012-10-09 12:15:28 -0700

    Defer translating system messages.
    Currently, messages are sent using the language of the sender, which
    doesn't work well for system messages. We're going to avoid translating
    them for now until we can return such messages in the language of the
    receiver.

--- r2/r2/lib/system_messages.py
+++ r2/r2/lib/system_messages.py
@@ -21,7 +21,7 @@
 ###############################################################################
 
 from pylons import request
-from pylons.i18n import _
+from pylons.i18n import N_
 
 from r2.models import Account, Message
 from r2.lib.db import queries
@@ -30,38 +30,38 @@ from r2.lib.db import queries
 user_added_messages = {
     "moderator_invite": {
         "pm": {
-            "subject": _("invitation to moderate %(url)s"),
-            "msg": _("**gadzooks! you are invited to become a moderator of [%(title)s](%(url)s)!**\n\n"
-                     "*to accept*, visit the [moderators page for %(url)s](%(url)s/about/moderators) and click \"accept\".\n\n"
-                     "*otherwise,* if you did not expect to receive this, you can simply ignore this invitation or report it."),
+            "subject": N_("invitation to moderate %(url)s"),
+            "msg": N_("**gadzooks! you are invited to become a moderator of [%(title)s](%(url)s)!**\n\n"
+                      "*to accept*, visit the [moderators page for %(url)s](%(url)s/about/moderators) and click \"accept\".\n\n"
+                      "*otherwise,* if you did not expect to receive this, you can simply ignore this invitation or report it."),
         },
         "modmail": {
-            "subject": _("moderator invited"),
-            "msg": _("%(user)s has been invited by %(author)s to moderate %(url)s."),
+            "subject": N_("moderator invited"),
+            "msg": N_("%(user)s has been invited by %(author)s to moderate %(url)s."),
         },
     },
     "accept_moderator_invite": {
         "modmail": {
-            "subject": _("moderator added"),
-            "msg": _("%(user)s has accepted an invitation to become moderator of %(url)s."),
+            "subject": N_("moderator added"),
+            "msg": N_("%(user)s has accepted an invitation to become moderator of %(url)s."),
         },
     },
     "contributor": {
         "pm": {
-            "subject": _("you are an approved submitter"),
-            "msg": _("you have been added as an approved submitter to [%(title)s](%(url)s)."),
+            "subject": N_("you are an approved submitter"),
+            "msg": N_("you have been added as an approved submitter to [%(title)s](%(url)s)."),
         },
     },
     "banned": {
         "pm": {
-            "subject": _("you've been banned"),
-            "msg": _("you have been banned from posting to [%(title)s](%(url)s)."),
+            "subject": N_("you've been banned"),
+            "msg": N_("you have been banned from posting to [%(title)s](%(url)s)."),
         },
     },
     "traffic": {
         "pm": {
-            "subject": _("you can view traffic on a promoted link"),
-            "msg": _('you have been added to the list of users able to see [traffic for the sponsored link "%(title)s"](%(traffic_url)s).'),
+            "subject": N_("you can view traffic on a promoted link"),
+            "msg": N_('you have been added to the list of users able to see [traffic for the sponsored link "%(title)s"](%(traffic_url)s).'),
         },
     },
 }
