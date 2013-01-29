1ae3fad1d2c86e4aada167d0fde5eeec23d9b8a5
Neil Williams <neil@reddit.com>
2013-01-28 21:05:37 -0800

    health: Clean up imports / dead code.

--- r2/r2/controllers/health.py
+++ r2/r2/controllers/health.py
@@ -20,32 +20,25 @@
 # Inc. All Rights Reserved.
 ###############################################################################
 
-from threading import Thread
-import os
-import time
 import json
 
-from pylons.controllers.util import abort
-from pylons import c, g, response
+from pylons import g, response
 
-from reddit_base import MinimalController
-from r2.lib.amqp import worker
-from r2.lib.validator import *
+from r2.controllers.reddit_base import MinimalController
 from r2.lib import promote
 
 
 class HealthController(MinimalController):
-    def post(self):
-        pass
-
     def try_pagecache(self):
         pass
 
     def pre(self):
         pass
 
+    def post(self):
+        pass
+
     def GET_health(self):
-        c.dontcache = True
         response.headers['Content-Type'] = 'text/plain'
         return json.dumps(g.versions, sort_keys=True, indent=4)
 
