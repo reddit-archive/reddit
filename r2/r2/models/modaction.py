38ae79be7ec919553a6bcb329ba574366e4b05cd
Chad Birch <chad.birch@gmail.com>
2013-07-24 14:12:33 -0600

    Mod log: remove final public 'ham' usage

--- r2/r2/models/modaction.py
+++ r2/r2/models/modaction.py
@@ -115,7 +115,7 @@ class ModAction(tdb_cassandra.UuidThing, Printable):
 
     _details_text = {# approve comment/link
                      'unspam': _('unspam'),
-                     'confirm_ham': _('confirmed ham'),
+                     'confirm_ham': _('approved'),
                      # remove comment/link
                      'confirm_spam': _('confirmed spam'),
                      'remove': _('removed not spam'),
