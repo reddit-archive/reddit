3e699d52533bf18a78e709970046cb5845a75bb8
Chad Birch <chad.birch@gmail.com>
2013-08-19 11:30:15 -0600

    Gold partners: add UPS

--- r2/r2/public/static/js/gold.js
+++ r2/r2/public/static/js/gold.js
@@ -188,7 +188,7 @@ r.gold = {
         return false
     },
     
-    claim_gold_partner_deal_code: function (elem, name) {
+    claim_gold_partner_deal_code: function (elem, name, redirect_url) {
         $.ajax({
                   type: 'POST',
                   dataType: 'json',
@@ -199,9 +199,13 @@ r.gold = {
                           var $newelem = $('<span class="error">').text(data['explanation'])
                           $(elem).replaceWith($newelem)
                       } else {
-                          var $newelem = $('<input type="text" class="code" readonly="readonly">').attr('value', data['code'])
-                          $(elem).replaceWith($newelem)
-                          $newelem.select()
+                          if (redirect_url) {
+                              window.location.href = redirect_url.replace('{{code}}', data['code'])
+                          } else {
+                              var $newelem = $('<input type="text" class="code" readonly="readonly">').attr('value', data['code'])
+                              $(elem).replaceWith($newelem)
+                              $newelem.select()
+                          }
                       }
                   }
                 })
