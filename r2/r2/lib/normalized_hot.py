380140f0aa35c243fc1c0313c9f85f9eccf7b30a
Brian Simpson <bsimpson63@gmail.com>
2013-11-13 14:42:03 -0500

    Use sgm to cache each subreddit's normalized_hot links.

--- r2/r2/lib/normalized_hot.py
+++ r2/r2/lib/normalized_hot.py
@@ -20,27 +20,66 @@
 # Inc. All Rights Reserved.
 ###############################################################################
 
+import heapq
+import itertools
+from datetime import datetime, timedelta
+
 from pylons import g
 
-from r2.lib.memoize import memoize
 from r2.lib import _normalized_hot
+from r2.lib.cache import sgm
+from r2.lib.db.queries import _get_links, CachedResults
+from r2.lib.db.sorts import epoch_seconds
 
 from r2.lib._normalized_hot import get_hot # pull this into our namespace
 
-@memoize('normalize_hot', time = g.page_cache_time)
-def normalized_hot_cached(sr_ids, obey_age_limit=True):
-    return _normalized_hot.normalized_hot_cached(sr_ids, obey_age_limit)
 
-def l(li):
-    if isinstance(li, list):
-        return li
-    else:
-        return list(li)
+MAX_PER_SUBREDDIT = 150
+MAX_LINKS = 1000
+
+
+def get_hot_tuples(sr_ids):
+    queries_by_sr_id = {sr_id: _get_links(sr_id, sort='hot', time='all')
+                        for sr_id in sr_ids}
+    CachedResults.fetch_multi(queries_by_sr_id.values())
+    tuples_by_srid = {sr_id: [] for sr_id in sr_ids}
+
+    for sr_id, q in queries_by_sr_id.iteritems():
+        if not q.data:
+            continue
+
+        link_name, hot, timestamp = q.data[0]
+        thot = max(hot, 1.)
+        tuples_by_srid[sr_id].append((-1., -hot, link_name, timestamp))
+
+        for link_name, hot, timestamp in q.data[1:MAX_PER_SUBREDDIT]:
+            ehot = hot / thot
+            # heapq.merge sorts from smallest to largest so we need to flip
+            # ehot and hot to get the hottest links first
+            tuples_by_srid[sr_id].append((-ehot, -hot, link_name, timestamp))
+
+    return tuples_by_srid
+
 
 def normalized_hot(sr_ids, obey_age_limit=True):
     timer = g.stats.get_timer("normalized_hot")
     timer.start()
-    sr_ids = l(sorted(sr_ids))
-    ret = normalized_hot_cached(sr_ids, obey_age_limit) if sr_ids else ()
+
+    if not sr_ids:
+        return []
+
+    tuples_by_srid = sgm(g.cache, sr_ids, miss_fn=get_hot_tuples,
+                         prefix='normalized_hot', time=g.page_cache_time)
+
+    if obey_age_limit:
+        cutoff = datetime.now(g.tz) - timedelta(days=g.HOT_PAGE_AGE)
+        oldest = epoch_seconds(cutoff)
+    else:
+        oldest = 0.
+
+    merged = heapq.merge(*tuples_by_srid.values())
+    generator = (link_name for ehot, hot, link_name, timestamp in merged
+                           if timestamp > oldest)
+    ret = list(itertools.islice(generator, MAX_LINKS))
     timer.stop()
     return ret
