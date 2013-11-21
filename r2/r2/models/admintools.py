ae8359c4bd84a4ad48541b613a5fe136d2f4f8b9
Brian Simpson <bsimpson63@gmail.com>
2013-11-21 00:13:41 -0500

    accountid_from_paypalsubscription: find spam users.

--- r2/r2/models/admintools.py
+++ r2/r2/models/admintools.py
@@ -237,7 +237,7 @@ def accountid_from_paypalsubscription(subscr_id):
         return None
 
     q = Account._query(Account.c.gold_subscr_id == subscr_id,
-                       data=False)
+                       Account.c._spam == (True, False), data=False)
     l = list(q)
     if l:
         return l[0]._id
