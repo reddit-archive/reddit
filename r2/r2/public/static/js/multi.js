33430d7d850ba994939af0eeee5c3b6818ec91a4
Andre D <Andre@andred.ca>
2014-01-15 13:51:45 -0500

    multi: Fix js error on sr name hover.
    Should also fix the bubble not showing up in general.

--- r2/r2/public/static/js/multi.js
+++ r2/r2/public/static/js/multi.js
@@ -515,7 +515,7 @@ r.multi.SubscribeButton = Backbone.View.extend({
 })
 
 r.multi.MultiSubscribeBubble = r.ui.Bubble.extend({
-    className: 'multi-selector hover-bubble',
+    className: 'multi-selector hover-bubble anchor-right',
     template: _.template('<div class="title"><strong><%- title %></strong><a class="sr" href="/r/<%- sr_name %>">/r/<%- sr_name %></a></div><div class="throbber"></div>'),
     itemTemplate: _.template('<label><input class="add-to-multi" type="checkbox" data-path="<%- path %>" <%- checked %>><%- name %><a href="<%- path %>" target="_blank" title="<%- open_multi %>">&rsaquo;</a></label>'),
     itemCreateTemplate: _.template('<label><form class="create-multi"><input type="text" class="multi-name" placeholder="<%- create_msg %>"><div class="error create-multi-error"></div></form></label>'),
