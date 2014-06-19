46167ecb88445b499ab675ee60480147b8bd9767
Ricky Ramirez <rram@reddit.com>
2014-06-19 15:53:39 -0700

    pages: Use a hashed key for commentpane.
    The previous method could generate key names with whitespace which don't work
    with the memcache ASCII protocol.

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -135,6 +135,7 @@ from r2.lib.utils import trunc_string as _truncate, to_date
 from r2.lib.filters import safemarkdown
 from r2.lib.utils import Storage, tup, url_is_embeddable_image
 from r2.lib.utils import precise_format_timedelta
+from r2.lib.cache import make_key
 
 from babel.numbers import format_currency
 from babel.dates import format_date
@@ -1632,15 +1633,24 @@ class CommentPane(Templated):
             num = (num / 100) * 100
         elif num > 100:
             num = (num / 10) * 10
-        return "_".join(map(str, ["commentpane", self.article._fullname,
-                                  self.article.contest_mode,
-                                  num, self.sort, self.num, c.lang,
-                                  self.can_reply, c.render_style,
-                                  c.domain_prefix, c.secure,
-                                  c.user.pref_show_flair,
-                                  c.user.pref_show_link_flair,
-                                  c.can_save,
-                                  self.max_depth, c.user.pref_highlight_controversial]))
+        return make_key(
+            "commentpane",
+            self.article._fullname,
+            self.article.contest_mode,
+            num,
+            self.sort,
+            self.num,
+            c.lang,
+            self.can_reply,
+            c.render_style,
+            c.domain_prefix,
+            c.secure,
+            c.user.pref_show_flair,
+            c.user.pref_show_link_flair,
+            c.can_save,
+            self.max_depth,
+            c.user.pref_highlight_controversial,
+        )
 
     def __init__(self, article, sort, comment, context, num, **kw):
         # keys: lang, num, can_reply, render_style
