54a710b89e789e961ebd3f5aa803b92c86dd658e
Brian Simpson <bsimpson63@gmail.com>
2014-07-29 16:30:23 -0400

    Add cpm_selfserve_collection.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -268,6 +268,7 @@ class Globals(object):
             'cpm_selfserve',
             'cpm_selfserve_geotarget_country',
             'cpm_selfserve_geotarget_metro',
+            'cpm_selfserve_collection',
         ],
     }
 
