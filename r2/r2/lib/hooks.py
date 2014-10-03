bd8e12c18c9eb3c3c87432b084e7c6ac47ae803d
xiongchiamiov <xiong.chiamiov@gmail.com>
2014-10-03 11:40:29 -0700

    Hooks: add a little bit of user documentation
    I wasn't sure how the system worked, so I poked around a bit and came up with
    a description, with a little help from Neil.

--- r2/r2/lib/hooks.py
+++ r2/r2/lib/hooks.py
@@ -19,7 +19,32 @@
 # All portions of the code written by reddit are Copyright (c) 2006-2014 reddit
 # Inc. All Rights Reserved.
 ###############################################################################
-"""A very simple system for event hooks for plugins etc."""
+"""A very simple system for event hooks for plugins etc.
+
+In general, you will probably want to use a ``HookRegistrar`` to manage your
+hooks.  The file that contains the code you want to hook into will look
+something like this::
+
+    from r2.lib import hooks
+    
+    def foo(spam):
+        # Do a little bit of this and a little bit of that.
+        eggs = this(spam)
+        baked_beans = that(eggs)
+    
+        hooks.get_hook('foo').call(ingredient=baked_beans)
+
+Then, any place you want to hook into it, just throw on a decorator::
+
+    from r2.lib.hooks import HookRegistrar
+    hooks = HookRegistrar()
+    
+    @hooks.on('foo')
+    def bar(ingredient):
+        print ingredient
+
+    hooks.register_all()
+"""
 
 
 _HOOKS = {}
