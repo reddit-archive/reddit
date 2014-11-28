88c17cec1b750a93b81a7dc44a061f39bbadf94b
Jordan Milne <jordan.milne@saynotolinux.com>
2014-11-28 14:41:26 -0400

    Guard against trying the pagecache twice in one request

--- r2/r2/lib/base.py
+++ r2/r2/lib/base.py
@@ -70,7 +70,12 @@ class BaseController(WSGIController):
 
     def __before__(self):
         self.pre()
-        self.try_pagecache()
+        # This guards against checking the pagecache twice and possibly
+        # modifying the request key when being redirected from one endpoint
+        # to the other in-request (i.e. when redirected to the error document)
+        if not c.tried_pagecache:
+            c.tried_pagecache = True
+            self.try_pagecache()
 
     def __after__(self):
         self.post()
