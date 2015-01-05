e0d5d77be9c88770af83d9ef8e3a021be8485efa
Brian Simpson <bsimpson63@gmail.com>
2015-01-05 14:28:58 -0500

    Remove scrollupdater debug statements.

--- r2/r2/public/static/js/scrollupdater.js
+++ r2/r2/public/static/js/scrollupdater.js
@@ -90,8 +90,6 @@
             var endTime = new Date()
             this._totalTime += endTime - startTime
 
-            r.debug('scrollupdater queued', count, 'in', endTime - startTime, 'ms')
-
             this._doUpdates()
         },
 
@@ -116,8 +114,6 @@
             }
 
             this._totalTime += endTime - startTime
-            r.debug('scrollupdater updated', count, 'in', endTime - startTime, 'ms')
-            r.debug('scrollupdater total', this._totalTime, 'ms')
 
             if (this._toUpdate.length) {
                 _.defer($.proxy(this, '_doUpdates'))
