1837221fc6909f16bb88b39a991abd3a73e0d0ad
Mirclus <Mirclus@users.noreply.github.com>
2015-04-09 17:49:02 +0200

    Add Date-Header to mails
    Formated regarding RFC 822, RFC 1036 and RFC 1123 by python email.utils

--- r2/r2/models/mail_queue.py
+++ r2/r2/models/mail_queue.py
@@ -22,6 +22,8 @@
 
 import datetime
 import hashlib
+import time
+import email.utils
 from email.MIMEText import MIMEText
 from email.errors import HeaderParseError
 
@@ -433,6 +435,8 @@ class Email(object):
             msg['To']      = utf8(self.to_addr)
             msg['From']    = utf8(fr)
             msg['Subject'] = utf8(self.subject)
+            timestamp = time.mktime(self.date.timetuple())
+            msg['Date'] = utf8(email.utils.formatdate(timestamp))
             if self.user:
                 msg['X-Reddit-username'] = utf8(self.user.name)
             msg['X-Reddit-ID'] = self.msg_hash
