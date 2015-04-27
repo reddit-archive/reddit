912b224a1b85e2d2c55bd8df2b333beb3fdc87eb
Jordan Milne <jordan.milne@saynotolinux.com>
2015-04-27 12:51:21 -0700

    Make sure special pages can only be created by mods with config perms
    Thanks to a report by @pokechu022. Previously we were only checking
    if the user was a moderator with permission to moderate the wiki.
    
    This didn't affect anything until the automoderator config page was
    added, as all other special pages are autocreated and don't pass
    the `may_not_create` check for _any_ user.

--- r2/r2/lib/validator/wiki.py
+++ r2/r2/lib/validator/wiki.py
@@ -320,7 +320,8 @@ class VWikiPageRevise(VWikiPage):
                 WikiPage.is_automatically_created(page)):
             return {'reason': 'PAGE_CREATED_ELSEWHERE'}
         elif WikiPage.is_special(page):
-            if not c.is_wiki_mod:
+            if not (c.user_is_admin or
+                    c.site.is_moderator_with_perms(c.user, 'config')):
                 self.set_error('RESTRICTED_PAGE', code=403)
                 return
         elif (not c.user_is_admin) and WikiPage.is_restricted(page):
