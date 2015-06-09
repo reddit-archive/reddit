43162e25e80f1edbfcdbfd8a11d72c4a0ff2e666
Chris Stephens <chrisst@gmail.com>
2015-06-09 10:53:29 -0700

    Refactor the image preview generation method
    This will allow the method to be called from other locations so we can
    use the image preview object in other locations.

--- r2/r2/lib/jsontemplates.py
+++ r2/r2/lib/jsontemplates.py
@@ -686,6 +686,40 @@ class LinkJsonTemplate(ThingJsonTemplate):
             return not thing.votable
         return ThingJsonTemplate.thing_attr(self, thing, attr)
 
+    @staticmethod
+    def generate_image_links(preview_object, censor_nsfw=False):
+        # Determine which previews would be feasible with our given dims
+        source_width = preview_object['width']
+        source_height = preview_object['height']
+        source_ratio = float(source_height) / source_width
+
+        preview_resolutions = []
+        for w in LinkJsonTemplate.PREVIEW_RESOLUTIONS:
+            if w > source_width:
+                continue
+
+            url = g.image_resizing_provider.resize_image(
+                preview_object, w, censor_nsfw,
+                LinkJsonTemplate.PREVIEW_MAX_RATIO)
+            h = int(w * source_ratio)
+            preview_resolutions.append({
+                "url": url,
+                "width": w,
+                "height": h,
+            })
+
+        url = g.image_resizing_provider.resize_image(
+            preview_object, censor_nsfw=censor_nsfw)
+
+        return {
+            "source": {
+                "url": url,
+                "width": source_width,
+                "height": source_height,
+            },
+            "resolutions": preview_resolutions,
+        }
+
     def raw_data(self, thing):
         d = ThingJsonTemplate.raw_data(self, thing)
 
@@ -697,45 +731,14 @@ class LinkJsonTemplate(ThingJsonTemplate):
 
         preview_object = getattr(thing, 'preview_object', None)
         if feature.is_enabled('link_preview') and preview_object:
-            source_width = preview_object['width']
-            source_height = preview_object['height']
-            source_ratio = float(source_height) / source_width
-
-            def generate_image_links(censor_nsfw=False):
-                # Determine which previews would be feasible with our given dims
-                preview_resolutions = []
-                for w in self.PREVIEW_RESOLUTIONS:
-                    if w > source_width:
-                        continue
-
-                    url = g.image_resizing_provider.resize_image(
-                        preview_object, w, censor_nsfw, self.PREVIEW_MAX_RATIO)
-                    h = int(w * source_ratio)
-                    preview_resolutions.append({
-                        "url": url,
-                        "width": w,
-                        "height": h,
-                    })
-
-                d['post_hint'] = thing.post_hint
-                url = g.image_resizing_provider.resize_image(
-                    preview_object, censor_nsfw=censor_nsfw)
-
-                return {
-                    "source": {
-                        "url": url,
-                        "width": source_width,
-                        "height": source_height,
-                    },
-                    "resolutions": preview_resolutions,
-                }
-
             d['preview'] = {}
-            images = generate_image_links()
+            d['post_hint'] = thing.post_hint
+            images = self.generate_image_links(preview_object)
             images['id'] = preview_object['uid']
             images['variants'] = {}
             if thing.nsfw:
-                images['variants']['nsfw'] = generate_image_links(censor_nsfw=True)
+                images['variants']['nsfw'] = self.generate_image_links(
+                    preview_object, censor_nsfw=True)
             d['preview']['images'] = [images]
 
         return d
