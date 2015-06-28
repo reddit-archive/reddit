ef2b8e8e897a3ef8cb1b98cd95661aee8fba0e5e
Brian Simpson <bsimpson63@gmail.com>
2015-06-28 13:31:05 -0400

    SubscriptionsBySubreddit: decrease memoization to 1 hour.
    We now write the data every hour (previously was once per day).

--- r2/r2/models/traffic.py
+++ r2/r2/models/traffic.py
@@ -642,7 +642,7 @@ class SubscriptionsBySubreddit(Base):
     subscriber_count = Column("unique", Integer())
 
     @classmethod
-    @memoize_traffic(time=3600 * 6)
+    @memoize_traffic(time=3600)
     def history(cls, interval, subreddit):
         time_points, q = make_history_query(cls, interval)
         q = q.filter(cls.subreddit == subreddit)
