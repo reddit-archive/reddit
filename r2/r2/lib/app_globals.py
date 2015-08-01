1d8909b82ca37689f52d613b7060d2d857453931
Brian Simpson <bsimpson63@gmail.com>
2015-07-31 23:23:46 -0400

    Switch rel memcache pool from binary to ascii protocol.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -677,7 +677,6 @@ class Globals(object):
             self.relcaches,
             min_compress_len=96,
             num_clients=num_mc_clients,
-            binary=True,
             validators=[validate_size_error],
         )
 
