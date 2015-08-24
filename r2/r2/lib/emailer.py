03e4a1876f4e844fba63bca1783ed46d8356d440
Chad Birch <chad.birch@gmail.com>
2015-08-24 11:45:58 -0600

    Password reset emails: attach to relevant account
    Emails are only getting associated with a specific account if they're
    queued up due to an action by a logged-in user. This is impossible with
    password-reset requests and the subsequent email for a successful
    change, but those should definitely be associated with the account that
    is being reset.

--- r2/r2/lib/emailer.py
+++ r2/r2/lib/emailer.py
@@ -41,15 +41,17 @@ from r2.models.token import EmailVerificationToken, PasswordResetToken
 trylater_hooks = hooks.HookRegistrar()
 
 def _system_email(email, body, kind, reply_to = "", thing = None,
-                  from_address = g.feedback_email):
+                  from_address=g.feedback_email, user=None):
     """
     For sending email from the system to a user (reply address will be
     feedback and the name will be reddit.com)
     """
-    Email.handler.add_to_queue(c.user if c.user_is_loggedin else None,
-                               email, g.domain, from_address,
-                               kind, body = body, reply_to = reply_to,
-                               thing = thing)
+    if user is None and c.user_is_loggedin:
+        user = c.user
+    Email.handler.add_to_queue(user,
+        email, g.domain, from_address, kind,
+        body=body, reply_to=reply_to, thing=thing,
+    )
 
 def _nerds_email(body, from_name, kind):
     """
@@ -123,7 +125,9 @@ def password_email(user):
     _system_email(user.email,
                   PasswordReset(user=user,
                                 passlink=passlink).render(style='email'),
-                  Email.Kind.RESET_PASSWORD)
+                  Email.Kind.RESET_PASSWORD,
+                  user=user,
+                  )
     return True
 
 @trylater_hooks.on('trylater.message_notification_email')
@@ -203,7 +207,9 @@ def password_change_email(user):
 
     return _system_email(user.email,
                          PasswordChangeEmail(user=user).render(style='email'),
-                         Email.Kind.PASSWORD_CHANGE)
+                         Email.Kind.PASSWORD_CHANGE,
+                         user=user,
+                         )
 
 def email_change_email(user):
     """Queues a system email for a email change notification."""
