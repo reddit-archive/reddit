e62b836c4cc94150d34b5d579d52ffc873fe4244
Florence Yeun <florwat@reddit.com>
2015-09-09 14:23:32 -0700

    Modmail: Fix missing color coding
    Add color coding to message threads collapsed with the
    "load the full conversation" link.
    
    Also rename `item.bar_color` to `item.accent_color`.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -1648,6 +1648,10 @@ class MoreMessages(Printable):
     def subreddit(self):
         return self.parent.subreddit
 
+    @property
+    def accent_color(self):
+        return getattr(self.parent, "accent_color", None)
+
 
 class MoreComments(Printable):
     cachable = False
@@ -1992,7 +1996,7 @@ class Message(Thing, Printable):
                 user_is_moderator = item.sr_id in user_mod_sr_ids
 
                 if sr_colors and user_is_moderator:
-                    item.bar_color = sr_colors.get(item.sr_id)
+                    item.accent_color = sr_colors.get(item.sr_id)
 
                 if item.subreddit.is_muted(item.author):
                     item.sr_muted = True
