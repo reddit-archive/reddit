047ae1c32d1bfe3d143195897082a2ee5a03a21e
MelissaCole <melissa.engineer@gmail.com>
2015-11-04 16:21:18 -0800

    Add link and icons for rules in mod tools
    This will add a link to /about/rules in the mod tools box and
    icons for create, edit, adn delete rules in the mod log.

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -523,6 +523,11 @@ class Reddit(Templated):
                 css_class="edit-stylesheet access-required",
                 dest="stylesheet",
                 data=data_attrs('stylesheet')))
+            buttons.append(NavButton(
+                menu.community_rules,
+                css_class="community-rules access-required",
+                dest="rules",
+                data=data_attrs('rules')))
 
         if is_moderator_with_perms('mail'):
             buttons.append(NamedButton(
