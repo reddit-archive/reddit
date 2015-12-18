813d31f6a4f1d338958db8d77644477e75ed59a7
David Wick <wick.d.spencer@gmail.com>
2015-12-18 10:52:00 -0800

    GTM Jail: Proxy messages in both directions.

--- r2/r2/public/static/js/google-tag-manager/gtm-jail-listener.js
+++ r2/r2/public/static/js/google-tag-manager/gtm-jail-listener.js
@@ -1,5 +1,5 @@
 ;(function(global, r, undefined) {
   var jail = document.getElementById('jail');
 
-  r.frames.proxy('gtm', jail.contentWindow);
+  r.frames.proxy('gtm', [jail.contentWindow, window.parent]);
 })(this, this.r);
