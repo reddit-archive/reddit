75143f2b5fde135fd74b5ecdf9419a50892be455
zeantsoi <zeantsoi@gmail.com>
2016-01-13 14:42:21 -0800

    Set sandbox permissions for redditgifts-embed iframe

--- r2/r2/controllers/promotecontroller.py
+++ r2/r2/controllers/promotecontroller.py
@@ -1059,13 +1059,24 @@ class PromoteApiController(ApiController):
                     form.set_error(errors.BAD_URL, "gifts_embed_url")
                     return
 
+                sandbox = (
+                    'allow-popups',
+                    'allow-forms',
+                    'allow-same-origin',
+                    'allow-scripts',
+                )
+                iframe_attributes = {
+                    'embed_url': websafe(iframe_embed_url),
+                    'sandbox': ' '.join(sandbox),
+                }
                 iframe = """
                     <iframe class="redditgifts-embed"
                             src="%(embed_url)s"
                             width="710" height="500" scrolling="no"
-                            frameborder="0" allowfullscreen>
+                            frameborder="0" allowfullscreen
+                            sandbox="%(sandbox)s">
                     </iframe>
-                """ % {'embed_url': websafe(gifts_embed_url)}
+                """ % iframe_attributes
                 media_object = {
                     'oembed': {
                         'description': 'redditgifts embed',
