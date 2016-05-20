9d161a9115b889688ae8d1ad595c3b3476b0ae02
Brian Simpson <bsimpson63@gmail.com>
2016-05-20 13:50:53 -0700

    Read Comment objects from g.thingcache

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -898,7 +898,7 @@ class Globals(object):
         self.comment_transitionalcache = TransitionalCache(
             original_cache=self.cache,
             replacement_cache=self.thingcache,
-            read_original=True,
+            read_original=False,
             key_transform=get_new_comment_prefix_and_key,
         )
 
