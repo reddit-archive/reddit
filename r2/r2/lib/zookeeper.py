50560d1b7dea8c94c035f3ef0a99af53a0bba1a0
Brian Simpson <bsimpson63@gmail.com>
2016-07-25 16:59:47 -0700

    zookeeper: Add ReducedLiveList and IPNetworkLiveList
    These classes are like LiveList (and use LiveList internally) but
    they also store and watch a copy of the reduced data. This is useful
    for cases where the map/reduce phase is slow and CPU intensive.
    By storing the reduced data separately the map/reduce only needs
    to be done by the process that's updating the list. All other processes
    watch the reduced data node.

--- r2/r2/lib/zookeeper.py
+++ r2/r2/lib/zookeeper.py
@@ -31,6 +31,7 @@ from kazoo.security import make_digest_acl
 from kazoo.exceptions import NoNodeException
 
 from r2.lib import hooks
+from r2.lib.contrib import ipaddress
 
 
 def connect_to_zookeeper(hostlist, credentials):
@@ -147,3 +148,87 @@ class LiveList(object):
     def __repr__(self):
         return "<LiveList %r (%s)>" % (self.data,
                                        "push" if self.is_watching else "pull")
+
+
+class ReducedLiveList(object):
+    """Store a copy of the reduced data in addition to the full LiveList.
+
+    This is useful for cases where the map/reduce phase is slow and CPU
+    intensive. By storing the reduced data separately the map/reduce only needs
+    to be done by the process that's updating the list. All other processes
+    watch the reduced data node.
+
+    """
+
+    def __init__(self, client, root, reduced_data_node, map_fn=None,
+                 reduce_fn=lambda L: L, to_json_fn=None, from_json_fn=None):
+        # don't watch the underlying LiveList because all updates are triggered
+        # on the reduced data node
+        self.live_list = LiveList(
+            client, root, map_fn=map_fn, reduce_fn=reduce_fn, watch=False)
+
+        self.client = client
+        self.root = root
+        self.reduced_data_node = reduced_data_node
+
+        acl = [self.client.make_acl(
+            read=True, write=True, create=True, delete=True)]
+        self.client.ensure_path(self.reduced_data_node, acl)
+
+        self.data = []
+        self.to_json_fn = to_json_fn
+        self.from_json_fn = from_json_fn
+
+        @client.DataWatch(self.reduced_data_node)
+        def watcher(json_data, stat):
+            if json_data and json_data.startswith("gzip"):
+                json_data = zlib.decompress(json_data[len("gzip"):])
+            self.data = self.from_json_fn(json_data or '{}')
+
+    def update(self):
+        data = self.live_list.get(reduce=True)
+        json_data = self.to_json_fn(data)
+        compressed_data = "gzip" + zlib.compress(json_data)
+        self.client.set(self.reduced_data_node, compressed_data)
+
+    def add(self, item):
+        self.live_list.add(item)
+        self.update()
+
+    def remove(self, item):
+        self.live_list.remove(item)
+        self.update()
+
+    def get(self, reduce=True):
+        if reduce:
+            return self.data
+        else:
+            return self.live_list.get(reduce=False)
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        return "<%s %r>" % (self.__class__.__name__, self.data)
+
+
+class IPNetworkLiveList(ReducedLiveList):
+    def __init__(self, client, root, reduced_data_node):
+        def ipnetwork_to_json(ipnetwork_list):
+            d = json.dumps([str(ipn) for ipn in ipnetwork_list])
+            return d
+
+        def json_to_ipnetwork(d):
+            ipnetwork_list = [ipaddress.ip_network(s) for s in json.loads(d)]
+            return ipnetwork_list
+
+        ReducedLiveList.__init__(
+            self, client, root, reduced_data_node,
+            map_fn=ipaddress.ip_network,
+            reduce_fn=ipaddress.collapse_addresses,
+            to_json_fn=ipnetwork_to_json,
+            from_json_fn=json_to_ipnetwork,
+        )
